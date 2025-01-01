017c7233d2c1dc2a76f8edb19a332bc32393fb2d
yorbil <barbe.rodolphe@gmail.com>
2025-01-01 23:41:46 +0100

    add pink garnet item with models states texture lang

--- src/main/java/net/yorbil/yorcraft/Yorcraft.java
+++ src/main/java/net/yorbil/yorcraft/Yorcraft.java
@@ -1,17 +1,12 @@
 package net.yorbil.yorcraft;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.yorbil.yorcraft.item.ModItems;
 
 public class Yorcraft implements ModInitializer {
-	public static final String MOD_ID = "yorcraft";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Yorbil!");
+		ModItems.initialize();
 	}
 }
---
+++ src/main/java/net/yorbil/yorcraft/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.yorbil.yorcraft.item;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", Item::new);
+    public static Item registerItem(String name) {
+        return registerItem(name, Item::new);
+    }
+    public static <T extends Item> T registerItem(String name, ItemFactory<T> factory) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("yorcraft",name));
+        return net.minecraft.registry.Registry.register(
+                Registries.ITEM,
+                key,
+                factory.create(new Item.Settings().registryKey(key))
+        );
+    }
+    @FunctionalInterface
+    public interface ItemFactory<T extends Item> {
+        T create(Item.Settings settings);
+    }
+    public static void initialize() {
+    }
+}
